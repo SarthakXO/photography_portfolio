e69896b780025ed6bc4e2fd75701238a36e655c5
Sarthak <sarthakbhatt@live.com>
2023-10-23 23:59:19 +0530

    added image slider

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,29 +1,61 @@
+"use client";
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
 import Image from "next/image";
-import React from "react";
+import { useState } from "react";
 // import { image } from "./sliderdata";
 
 const Slider = ({ slides }) => {
-  //   console.log(image[0]);
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
   return (
-    <div id="gallery">
-      <h1>Gallery</h1>
-      <div>
-        {slides.map((pic, index) => {
-          return (
-            <div key={index}>
-              <Image
-                src={pic.image}
-                alt="image"
-                width={1440}
-                height={600}
-                objectFit="cover"
+    <div id="gallery" className="max-w-[1240px] max-h-[600px] mx-auto">
+      <h1 className="text-2xl font-bold text-center p-4">Gallery</h1>
+
+      {slides.map((pic, index) => {
+        return (
+          <div
+            className={
+              index == current
+                ? `  opacity-[1] ease-in duration-300`
+                : "  opacity-0"
+            }
+            key={index}
+          >
+            <div className="flex relative justify-center p-4">
+              <FaArrowCircleLeft
+                size={50}
+                className="absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                onClick={prevSlide}
+              />
+              {index === current && (
+                <Image
+                  src={pic.image}
+                  alt="image"
+                  width={1440}
+                  height={600}
+                  objectFit="cover"
+                />
+              )}
+              <FaArrowCircleRight
+                size={50}
+                className="absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]"
+                onClick={nextSlide}
               />
             </div>
-          );
-        })}
+          </div>
+        );
+      })}
 
-        {/* <Image src="/img4.jpg" alt="image" width={500} height={500} /> */}
-      </div>
+      {/* <Image src="/img4.jpg" alt="image" width={500} height={500} /> */}
     </div>
   );
 };
--- components/sliderdata.js
+++ components/sliderdata.js
@@ -1,17 +1,22 @@
 export const image = [
   {
-    image: "/img6.jpg",
+    image:
+      "https://images.unsplash.com/photo-1615859131861-052f0641a60e?auto=format&fit=crop&q=80&w=2189&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: "/img7.jpg",
+    image:
+      "https://images.unsplash.com/photo-1533282960533-51328aa49826?auto=format&fit=crop&q=80&w=2142&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: "/img3.jpg",
+    image:
+      "https://images.unsplash.com/photo-1613663294172-4c14b96dc23b?auto=format&fit=crop&q=80&w=2128&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: "/img4.jpg",
+    image:
+      "https://images.unsplash.com/photo-1615003162333-d3ff3ce1f0f4?auto=format&fit=crop&q=80&w=2160&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    image: "/img5.jpg",
+    image:
+      "https://images.unsplash.com/photo-1517461480445-a4392351a417?auto=format&fit=crop&q=80&w=1942&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
